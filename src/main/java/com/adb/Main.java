388055bbbeaa03f3638e2037b2cbf111311161c5
adbertran <adbertran@gmail.com>
2021-04-13 20:46:16 -0500

    Agregue un post

--- src/main/java/com/adb/Main.java
+++ src/main/java/com/adb/Main.java
@@ -5,6 +5,6 @@ import static spark.Spark.*;
 public class Main {
     public static void main (String[] args){
         get("/hello",(req,res)->"Hello World");
-
+        post("/users/create",(req,res)->"Usuario Creado");
     }
 }
