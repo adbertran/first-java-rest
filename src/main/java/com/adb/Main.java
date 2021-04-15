d6143a290d66ceb45bd5fc4365cf907e3853b252
guitchox <guillermo.walther@gmail.com>
2021-04-14 21:44:57 -0300

    parametros en el get

--- src/main/java/com/adb/Main.java
+++ src/main/java/com/adb/Main.java
@@ -3,6 +3,8 @@ package com.adb;
 import spark.Request;
 import spark.Response;
 
+import javax.servlet.http.HttpServletResponse;
+
 import static spark.Spark.*;
 
 public class Main {
@@ -12,7 +14,13 @@ public class Main {
     }
 
     private static String getHello(Request req, Response res) {
-        return "Hello World";
+        if (req.queryParams("planet") == null) {
+            res.status(HttpServletResponse.SC_BAD_REQUEST);
+            return "Pusiste mal el parametro";
+        }
+        String planet=req.queryParams("planet");
+        res.status(HttpServletResponse.SC_OK);
+        return "Hello " + planet;
     }
 
     private static String postCreateUser(Request req, Response res) {
