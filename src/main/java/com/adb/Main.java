3587e3d17d40263507d91586e4b3898758d81900
guitchox <guillermo.walther@gmail.com>
2021-04-14 21:09:00 -0300

    Arregle el pom y extraje los metodos de los end point

--- src/main/java/com/adb/Main.java
+++ src/main/java/com/adb/Main.java
@@ -1,10 +1,21 @@
 package com.adb;
 
+import spark.Request;
+import spark.Response;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main (String[] args){
-        get("/hello",(req,res)->"Hello World");
-        post("/users/create",(req,res)->"Usuario Creado");
+        get("/hello", Main::getHello);
+        post("/users/create", Main::postCreateUser);
+    }
+
+    private static String getHello(Request req, Response res) {
+        return "Hello World";
+    }
+
+    private static String postCreateUser(Request req, Response res) {
+        return "Usuario Creado";
     }
 }
