21a3bf7f7d8d09174663648aa25f6543f475f89c
Gustavo D'Andrea <gdandrea@gmail.com>
2021-04-21 20:37:40 -0300

    updateed POM

--- src/main/java/com/adb/utils/JsonResponseFactory.java
+++ src/main/java/com/adb/utils/JsonResponseFactory.java
@@ -5,5 +5,5 @@ public class JsonResponseFactory {
     //todo createErrorResponse(response, status code, descripcion)
     //todo createSuccessResponse(response, descripcion)
     //todo createJsonResponse(response, status code, objeto)
-        //todo response tipo json, convertir objeto en string
+    //todo response tipo json, convertir objeto en string
 }
